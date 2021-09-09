dedd8ea4a3935ae1d8ec4b0ba55b451e31d84bdf
Trent Garner <trentongarner.13@icloud.com>
2021-09-09 13:26:09 -0600

    Add auth methods create and destroy sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root 'courses#index'
   get 'about', to: 'pages#about'
   get 'courses/new', to: 'courses#new'
+  get 'login', to: 'logins#new'
+  post 'login', to: 'logins#create'
+  delete 'logout', to: 'logins#destroy'
   resources :students, except: [:destroy]
 end
